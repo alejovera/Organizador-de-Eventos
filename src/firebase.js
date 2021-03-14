5c825b98325961f654771c73d848a1748500223e
Alejo <vera.alejo0903@gmail.com>
2021-03-14 17:21:28 -0300

    Primeros archivos

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyARnihfXVk0nTmiX38w8nRjYRh8eT4F5EI",
+    authDomain: "organizador-eventos0.firebaseapp.com",
+    projectId: "organizador-eventos0",
+    storageBucket: "organizador-eventos0.appspot.com",
+    messagingSenderId: "780557691937",
+    appId: "1:780557691937:web:ae74155d6bb834451c8760",
+    measurementId: "G-B4RYT8ZB86"
+  };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
